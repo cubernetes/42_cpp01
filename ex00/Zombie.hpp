ba6ef2c7cb2557619fb721d6a384484a80d00b77
cubernetes <timo42@proton.me>
2024-11-26 22:22:30 +0100

    explicitly mark zombie class members as private

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -3,11 +3,12 @@
 #include <string>
 
 class Zombie {
-	const std::string& _name;
 public:
 	Zombie(const std::string& name = "anonymous");
 	~Zombie();
 	void announce();
+private:
+	const std::string& _name;
 };
 
 Zombie* newZombie(std::string name);
